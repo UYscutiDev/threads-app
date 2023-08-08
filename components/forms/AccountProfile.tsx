5873b2f9aa691ef17d8e0fa3dafd2331c1c87a6e
Avshalom-Ts <avshalomz.ts@gmail.com>
2023-08-08 21:13:33 +0300

    Created user function to create user in mongoDB

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -22,6 +22,8 @@ import { Textarea } from "@/components/ui/textarea";
 import { ChangeEvent, useState } from 'react';
 import { isBase64Image } from '@/lib/utils';
 import { useUploadThing } from '@/lib/uploadthing';
+import { updateUser } from '@/lib/actions/user.action';
+import { usePathname, useRouter } from 'next/navigation';
 
 interface Props {
   user: {
@@ -38,6 +40,8 @@ interface Props {
 const AccountProfile = ({ user, btnTitle }: Props) => {
   const [files, setFiles] = useState<File[]>([]);
   const { startUpload } = useUploadThing('media');
+  const router = useRouter();
+  const pathname = usePathname()
 
   const form = useForm({
     resolver: zodResolver(UserValidation),
@@ -70,7 +74,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
     }
   }
 
-  const onSubmit = async(values: z.infer<typeof UserValidation>) => {
+  const onSubmit = async (values: z.infer<typeof UserValidation>) => {
     const blob = values.profile_photo;
 
     const hasImageChanged = isBase64Image(blob);
@@ -83,7 +87,20 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
       }
     }
 
-    // TODO: Update the user profile in mongoDB
+    await updateUser({
+      userId: user.id,
+      username: values.username,
+      name: values.name,
+      bio: values.bio,
+      image: values.profile_photo,
+      path: pathname
+    });
+
+    if (pathname === '/profile/edit') {
+      router.back();
+    } else {
+      router.push('/');
+    }
   }
 
 
