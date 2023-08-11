257e0dff25c0659eb61d3b1d952fb30d358dfa94
Avshalom-Ts <avshalomz.ts@gmail.com>
2023-08-11 19:09:03 +0300

    Created the profille tab

---
+++ components/shared/ThreadsTab.tsx
@@ -0,0 +1,15 @@
+interface Props {
+  currentUserId: string,
+  accountId: string,
+  accoutType: string,
+}
+
+const ThreadsTab = async ({ currentUserId, accountId, accoutType }: Props) => {
+  return (
+    <section>
+      Threads Tab
+    </section>
+  )
+}
+
+export default ThreadsTab;
