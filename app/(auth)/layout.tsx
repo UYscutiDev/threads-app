d5f8be44b7ed1e626b421c814f0cd848167ecc78
Avshalom-Ts <avshalomz.ts@gmail.com>
2023-08-12 21:40:16 +0300

    Rendering the activities thread of a user in the activity page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,16 +7,18 @@ export const metadata = {
   description: 'A Next.js 13 Meta Threads Application'
 }
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <html lang="en">
         <body className={`${inter.className} bg-dark-1`}>
-          {children}
+          <div className="w-full flex justify-center items-center min-h-screen">
+            {children}
+          </div>
         </body>
-       </html>
+      </html>
     </ClerkProvider>
   )
 }
